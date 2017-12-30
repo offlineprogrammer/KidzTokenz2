95637e15dc0c2e41f9cf09ce18b98fb61ae09f78
offlineprogrammer <mmalaka@gmail.com>
2017-12-29 22:42:12 -0800

    Chart

--- src/pages/add-points/add-points.ts
+++ src/pages/add-points/add-points.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  Component,
+  ViewChild
+} from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams
+} from 'ionic-angular';
 import {
   Kid
 } from '../../models/kid';
 import {
   DataServiceProvider
 } from '../../providers/data-service/data-service';
+import {
+  Chart
+} from 'chart.js';
 
 /**
  * Generated class for the AddPointsPage page.
@@ -20,25 +30,72 @@ import {
   templateUrl: 'add-points.html',
 })
 export class AddPointsPage {
+  @ViewChild('doughnutCanvas') doughnutCanvas;
   oKid: Kid
-  constructor(public navCtrl: NavController, public navParams: NavParams,private dataService: DataServiceProvider) {
+  percentage: number;
+  doughnutChart: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private dataService: DataServiceProvider) {
     this.oKid = navParams.get('kid');
     console.log(this.oKid);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddPointsPage');
+    this.calculatePercentage();
   }
 
   addPositives(): void {
     console.log(this.oKid.positives);
-    
+
     this.oKid.positives++;
     console.log(this.oKid.positives);
+    this.calculatePercentage();
   }
+
+
+  calculatePercentage() {
+    console.log(this.oKid.positives);
+    this.percentage = Math.round(100 - ((this.oKid.negatives / this.oKid.positives) * 100));
+
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+
+      type: 'doughnut',
+      data: {
+        labels: [this.percentage + "% Positive"],
+        datasets: [{
+          data: [this.percentage, (100 - this.percentage)],
+          backgroundColor: [
+            'rgba(0, 255, 0, 1)',
+            'rgba(0, 0, 0, 0)'
+          ]
+        }]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        title: {
+          display: true,
+          text: this.percentage + "% Positive",
+          fontStyle: 'bold',
+          fontSize: 18
+      }
+      },
+
+    });
+
+    //this.oKid.positives++;
+    console.log(this.percentage);
+  }
+
 
   addNegatives(): void {
     this.oKid.negatives++;
+    this.calculatePercentage();
   }
 
   deleteKid(data: Kid): void {
